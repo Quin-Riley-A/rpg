aeefbf337ff7f843e5117d293cbf223b8032134f
Quin-Riley-A <quin.asselin1@gmail.com>
2022-10-03 22:11:52 -0400

    create initial environment from template
    Co-authored-by: Athea DeLing <almdeling@gmail.com>
    Co-authored-by: Grace Lee <gracexsnoopy@gmail.com>
    Co-authored-by: Greg Ramsower <greg.ramsower@gmail.com>
    Co-authored-by: Najma Abdi <anajmabdi@gmail.com>
    Co-authored-by: Niles O'Neil <nilesoneil@gmail.com>

---
+++ __tests__/[name].tests.js
@@ -0,0 +1,10 @@
+import [function] from 'source-path'
+
+describe('[function name]', () => {
+  test('should [do something]', () => {
+    //code
+    // const date = new Date('2022-10-01');
+    //expect statements
+    expect([value]).toEqual([othervalue]);
+  });
+});
