659b4e11d9dd460dc9c67eba4e606482a7c0496a
Quin-Riley-A <quin.asselin1@gmail.com>
2022-10-04 23:03:31 -0400

    successfully construct barbarian object w/test 1 super constructor
    Co-authored-by: Athea DeLing <almdeling@gmail.com>
    Co-authored-by: Grace Lee <gracexsnoopy@gmail.com>
    Co-authored-by: Greg Ramsower <greg.ramsower@gmail.com>
    Co-authored-by: Najma Abdi <anajmabdi@gmail.com>
    Co-authored-by: Niles O'Neil <nilesoneil@gmail.com>

--- __tests__/barbarian.tests.js
+++ __tests__/barbarian.tests.js
@@ -1,10 +1,8 @@
-import Character from './../src/character.js'
 import Barbarian from './../src/roles/barbarian.js'
 
 describe('Barbarian', () => {
   test('should build a Barbarian-class character with modified property values', () => {
-    let character = new Character(100, 20, 50, 50);
-    let barbarian = new Barbarian(character);
+    let barbarian = new Barbarian(100, 20, 50, 50);
     expect(barbarian.strength).toEqual(101);
     expect(barbarian.intelligence).toEqual(19);
     expect(barbarian.charisma).toEqual(50);
--- src/roles/barbarian.js
+++ src/roles/barbarian.js
@@ -2,11 +2,9 @@ import Character from './../character.js'
 export default class Barbarian extends Character {
   constructor(strength, intelligence, charisma, health) {
     super(strength, intelligence, charisma, health);
-    // this.strength = character.strength + 1;
-    // this.intelligence = character.intelligence - 1;
-    // this.charisma = character.charisma;
-    // this.health = character.health;
-    // this.role = "Barbarian";
+    this.strength += 1;
+    this.intelligence +=  -1;
+    this.role = "Barbarian";
   }
   //Barbarian methods
 }
